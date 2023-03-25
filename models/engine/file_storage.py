708ef078c2ed713eb1cb921394c5b538e4b7a0f5
johnNaduer <naduer789@gmail.com>
2023-03-25 09:29:56 -0700

    feat: add new code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -26,7 +32,7 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Loads storage dictionary from file
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -34,12 +40,13 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
-
+        """
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
                   }
+        
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -48,3 +55,4 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
