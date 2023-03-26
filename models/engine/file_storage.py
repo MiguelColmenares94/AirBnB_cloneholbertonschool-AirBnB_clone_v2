2a37a254aed05df3650c1d0c6370ba9d2e930b8b
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-03-25 20:23:27 -0700

    fix: create the dict classes that wasn't created in the original file but called

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,10 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+classes = {"BaseModel": BaseModel, "User": User, "State": State,
+                   "Amenity": Amenity, "Place": Place, "City": City,
+                   "Review": Review}
+
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
