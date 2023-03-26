149073846f626159098c14b1c396c33395d85135
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-03-25 22:59:18 -0700

    fix: add class that is inherited and declare getter properly

--- models/state.py
+++ models/state.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.city import City
 from os import getenv
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
+
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     class State(BaseModel, Base):
-        """ State class """
+        """ State class for DB"""
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state', cascade='all, delete')
 
 else:
-    @property
-    def cities(self):
-        """Returns the list of City instances"""
-        return[city
-               for city in storage.all(City).values()
-               if city.state_id == self.id]
+    class State(Basemodel):
+        """State class for FS"""
+
+       @property
+       def cities(self):
+           """Returns the list of City instances"""
+           return[city
+                  for city in storage.all(City).values()
+                  if city.state_id == self.id]
 
 
