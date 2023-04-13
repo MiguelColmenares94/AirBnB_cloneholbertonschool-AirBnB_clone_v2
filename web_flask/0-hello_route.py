ae2458e81f81061e99206456903d27d6c9b84240
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-04-13 09:28:08 -0700

    doc: document module and greet method according to PEP8 guidelines

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Script that starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @   app.route("/", strict_slashes=False)
 def greet():
+    """Greet anyone who see this URL"""
     return "Hello HBNB!"
