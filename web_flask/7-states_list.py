adf421b25c6f026d7b079aa0754bc2f43e5b7743
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-04-16 19:04:49 -0700

    feat: render an HTML page from a method

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from models import storage, storage.all
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def td_appcontext():
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    """Return an HTML page showing a list with all state objects"""
+    return render_template('7-states_list.html', states=storage.all.states.values())
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
