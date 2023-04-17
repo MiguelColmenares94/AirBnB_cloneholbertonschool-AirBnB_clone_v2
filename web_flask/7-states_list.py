f51ab1401e2e8e658bef6b625e009163a53a6cc8
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-04-16 20:58:48 -0700

    fix: test if the order of methods and decorators affect result

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,17 +7,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def td_appcontext(self):
-    """After each request remove the current SQLAlchemy Session"""
-    storage.close()
-
-
 @app.route("/states_list", strict_slashes=False)
 def state_list():
     """Return an HTML page showing a list with all state objects"""
     return render_template('7-states_list.html',
                            states=storage.all(State).values())
+
+
+@app.teardown_appcontext
+def td_appcontext(self):
+    """After each request remove the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == "__main__":
