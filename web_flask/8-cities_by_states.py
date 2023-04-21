ecfac09a8467af4ce046e23cb51103d7d825e0bc
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-04-20 20:26:17 -0700

    feat: Flask app that list all cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_list():
+    """Return an HTML page showing a list with all cities objects for every state"""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all(State).values())
+
+
+@app.teardown_appcontext
+def close_session(self):
+    """After each request remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
